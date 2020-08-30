2caa0c582138d8583b55c31530235a2ace936bb9
osostest <38068587+osostest@users.noreply.github.com>
2020-08-30 10:11:11 +0200

    Update upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
 
-# Assumes that an image is built via `run_docker.sh`
-
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
-
-# Step 2:  
-# Authenticate & tag
+dockerpath=mohamed_osama
 echo "Docker ID and Image: $dockerpath"
-
-# Step 3:
-# Push image to a docker repository
+docker login
+docker push $dockerpath
